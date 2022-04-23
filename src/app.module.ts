46a162d98690a10f1626ca5a10269df638283f58
Lucianna Almeida <lucianna.th@gmail.com>
2022-04-23 17:45:47 -0300

    graphql initial config and creating LessonModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // save the schema in memory and regenerate it everytime we start the nestjs application
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
 })
 export class AppModule {}
