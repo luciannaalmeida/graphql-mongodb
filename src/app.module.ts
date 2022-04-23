e0ad534ad09db7ea490339e0fcf8f8db4fee32d2
Lucianna Almeida <lucianna.th@gmail.com>
2022-04-23 19:22:25 -0300

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // save the schema in memory and regenerate it everytime we start the nestjs application
